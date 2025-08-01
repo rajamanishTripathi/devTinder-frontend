fbe1e41452e29cdc82b7efb123e7dd8d89eac691
Raja Manish <raghupatiraghav882@gmail.com>
2025-08-01 18:50:22 +0530

    refactoring code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { Provider } from "react-redux";
 
-import Body from "./Body";
-import Login from "./Login";
-import Profile from "./Profile";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 import appStore from "./utils/appstore";
-import Feed from "./Feed";
+import Feed from "./components/Feed";
 
 function App() {
 
@@ -14,15 +14,15 @@ function App() {
   return (
     <>
     <Provider store={appStore}>
-      <BrowserRouter basename="/">
-          <Routes>
-                     <Route path="/" element={<Body/>}>
-                      <Route path="/" element={<Feed/>}/>
-                      <Route path="/login" element={<Login/>}/>
-                      <Route path="/profile" element={<Profile/>}/>
-               </Route>
-          </Routes>
-      </BrowserRouter>
+        <BrowserRouter basename="/">
+            <Routes>
+                <Route path="/" element={<Body/>}>
+                  <Route path="/" element={<Feed/>}/>
+                  <Route path="/login" element={<Login/>}/>
+                  <Route path="/profile" element={<Profile/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
       </Provider>
     </>
   )
