d3d50a1491b054fb1d8c4454d3e9588f77f9b270
Raja Manish <raghupatiraghav882@gmail.com>
2025-08-02 19:18:05 +0530

    commiting again

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+
+const EditProfile = ({user}) => {
+
+    const [firstName, setFirstName] =useState(user.firstName);
+    const [lastName, setLastName] =useState(user.lastName);
+    const [photourl, setPhotourl] =useState(user.photourl);
+    const [age, setAge] =useState(user.age);
+    const [gender, setGender] =useState(user.gender);
+    const [skills, setSkills] =useState(user.skills);
+    const [about, setAbout] =useState(user.about);
+
+  return (
+    <div>
+        <div className='flex justify-center my-10'>
+        <div className="card card-dash bg-base-300 w-96">
+            <div className="card-body">
+                <h2 className="card-title flex justify-center">Edit Profile</h2>
+                <div>
+                    <div className='label'>
+                        <span className='label-text'>First Name</span>
+                    </div>
+                    <label className="input validator">
+                        <input type="text"
+                        value={firstName} 
+                        onChange={(e) => setFirstName(e.target.value)} />
+                    </label>
+                </div>
+                <div>
+                    <div className='label'>
+                        <span className='label-text'>Last Name</span>
+                    </div>
+                    <label className="input validator">
+                        <input type="text"
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}/>
+                    </label>
+                </div>
+                <div>
+                    <div className='label'>
+                        <span className='label-text'>Photo</span>
+                    </div>
+                    <label className="input validator">
+                        <input type="text"
+                        value={photourl}
+                        onChange={(e) => setPhotourl(e.target.value)} />
+                    </label>
+                </div>
+                <div>
+                    <div className='label'>
+                        <span className='label-text'>Age</span>
+                    </div>
+                    <label className="input validator">
+                        <input type="text"
+                        value={age}
+                        onChange={(e) => setAge(e.target.value)}/>
+                    </label>
+                </div>
+                <div>
+                    <div className='label'>
+                        <span className='label-text'>Gender</span>
+                    </div>
+                    <label className="input validator">
+                        <input type="text" 
+                        value={gender}
+                        onChange={(e) => setGender(e.target.value)} />
+                    </label>
+                </div>
+                <div>
+                    <div className='label'>
+                        <span className='label-text'>Skills</span>
+                    </div>
+                    <label className="input validator">
+                        <input type="text"
+                        value={skills} 
+                        onChange={(e) => setSkills(e.target.value)} />
+                    </label>
+                </div>
+                <div>
+                    <div className='label'>
+                        <span className='label-text'>About</span>
+                    </div>
+                    <label className="input validator">
+                        <input type="text" 
+                        value={about}
+                        onChange={(e) => setAbout(e.target.value)} />
+                    </label>
+                </div>
+                <div className="card-actions flex justify-center my-5">
+                    <button className="btn btn-primary" >Save</button>
+                </div>
+            </div>
+        </div>
+        </div>
+        <UserCard user={{}}/>
+    </div>
+  )
+};
+
+export default EditProfile;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import EditProfile from './EditProfile';
+import { useSelector } from 'react-redux';
 
 const Profile = () => {
-  return (
+  const user = useSelector((store) => store.user);
+  return user && (
     <div>
-      <div>Profile</div>
+      <EditProfile user={user}/>
     </div>
   )
 }
