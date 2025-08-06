043f89f87d20894b27e00199727719a16f430c2a
Raja Manish <raghupatiraghav882@gmail.com>
2025-08-06 10:38:39 +0530

    doing css changes

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -33,9 +33,23 @@ const Connections = () => {
     if(connections.length === 0){
         return <div>No Connection found..... </div>;
     }
-  return (
-    <div>
-      <div>Connections</div>
+  return connections && (
+    <div className='text-center my-10'>
+      <h1 className='m-10 text-center text-2xl'>Connections</h1>
+     { connections.map((connection) => {
+             const {firstName, lastName, photourl ,age ,gender,skills,about} = connection;
+             return (
+              <div className='flex m-4 p-2 bg-base-300 w-3xl m-auto my-5'>
+                <div><img  alt='photo' className='w-20 h-20 rounded-2xl' src={connection.photourl}/></div>
+                <div className='mx-4 '><h2 className='bold'>{firstName+' '+lastName}</h2>
+                   {age && gender && <div>{age+"  ,"+gender}</div>}
+                   <div>{skills}</div>
+                   <p>{about}</p>
+                </div>
+              </div>
+             )
+      })}
+      <div></div>
     </div>
   )
 }
